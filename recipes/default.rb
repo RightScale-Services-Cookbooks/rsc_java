0219795be97b83540fb8a87359be1b86f8b50001
Stefhen Hovland <stefhen@rightscale.com>
2015-01-06 15:16:17 -0500

    Add JDK version

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,6 +8,7 @@
 #
 
 node.override['java']['install_flavor'] = 'oracle'
+node.override['java']['jdk_version'] = '8'
 node.override['java']['oracle']['accept_oracle_download_terms'] = 'true'
 
 include_recipe 'java::default'
