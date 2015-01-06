a9731d5116b132fdcd414394bf290daa30d41fca
Stefhen Hovland <stefhen@rightscale.com>
2015-01-06 14:52:19 -0500

    Add overrides

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,8 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+node.override['java']['install_flavor'] = 'oracle'
+node.override['java']['oracle']['accept_oracle_download_terms'] = 'true'
+
+include_recipe 'java::default'
