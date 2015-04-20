4e340d659e71548b4cf91cfd958a2fc7eb5c52dd
Curt Wilhelm <curt_wilhelm@yahoo.com>
2015-04-20 15:40:46 -0600

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
 description      'Installs/Configures rsc_java'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '1.0.0'
 
 recipe 'rsc_java::default', 'Configure Java overrides'
 
